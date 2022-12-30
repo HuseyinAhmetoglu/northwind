ddd3e06d0d20a63d66c8bc3d34639d2cbe362f62
Huseyin Ahmetoglu <huseyinahmetoglu@artuklu.edu.tr>
2022-12-30 23:41:11 +0300

    commit

--- src/main/java/com/kodlamaio/northwind/business/concretes/ProductManager.java
+++ src/main/java/com/kodlamaio/northwind/business/concretes/ProductManager.java
@@ -1,12 +1,17 @@
 package com.kodlamaio.northwind.business.concretes;
 
 import com.kodlamaio.northwind.business.abstracts.ProductService;
+import com.kodlamaio.northwind.core.utilities.results.DataResult;
+import com.kodlamaio.northwind.core.utilities.results.Result;
+import com.kodlamaio.northwind.core.utilities.results.SuccessDataResult;
+import com.kodlamaio.northwind.core.utilities.results.SuccessResult;
 import com.kodlamaio.northwind.dataAccess.abstracts.ProductDao;
 import com.kodlamaio.northwind.entities.concretes.Product;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+
 @Service
 public class ProductManager implements ProductService {
 
@@ -18,7 +23,14 @@ public class ProductManager implements ProductService {
     }
 
     @Override
-    public List<Product> getAll() {
-        return this.productDao.findAll();
+    public DataResult<List<Product>> getAll() {
+        return new SuccessDataResult<List<Product>>(this.productDao.findAll(), "Data Listelendi");
+
+    }
+
+    @Override
+    public Result add(Product product) {
+        this.productDao.save(product);
+        return new SuccessResult("Ürün eklendi.");
     }
 }
