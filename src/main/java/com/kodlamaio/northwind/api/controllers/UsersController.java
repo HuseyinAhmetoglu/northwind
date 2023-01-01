28b9f6c2702012a1deb2549754238b459adf764b
Huseyin Ahmetoglu <huseyinahmetoglu@artuklu.edu.tr>
2023-01-01 19:33:01 +0300

    commit

--- src/main/java/com/kodlamaio/northwind/api/controllers/UsersController.java
+++ src/main/java/com/kodlamaio/northwind/api/controllers/UsersController.java
@@ -1,9 +1,44 @@
 package com.kodlamaio.northwind.api.controllers;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.kodlamaio.northwind.business.abstracts.UserService;
+import com.kodlamaio.northwind.core.entities.User;
+import com.kodlamaio.northwind.core.utilities.results.ErrorDataResult;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequestMapping(value = "/api/users")
 public class UsersController {
+
+    private UserService userService;
+
+    public UsersController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping(value = "/add")
+    public ResponseEntity<?> add(@RequestBody User user) {
+        return ResponseEntity.ok(this.userService.add(user));
+    }
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ErrorDataResult<Object> handleValidationException
+            (MethodArgumentNotValidException exception) {
+        Map<String, String> validationError = new HashMap<String, String>();
+        for (FieldError fieldError : exception.getBindingResult().getFieldErrors()) {
+            validationError.put(fieldError.getField(), fieldError.getDefaultMessage());
+        }
+
+        ErrorDataResult<Object> errors = new ErrorDataResult<Object>
+                (validationError, "Doğrulama Hataları");
+        return errors;
+    }
+
 }
